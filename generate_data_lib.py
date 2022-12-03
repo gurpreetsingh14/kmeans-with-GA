1f481b1a276c90c0e22803b086de30b586c41b21
gurpreetsingh14 <gps.off.14@gmail.com>
2022-12-03 14:47:36 -0500

    test

--- generate_data_lib.py
+++ generate_data_lib.py
@@ -10,7 +10,7 @@ import numpy
 import random
 import matplotlib.pyplot
 
-class generate_data:
+class GenerateData:
     '''
     A class to generate and plot data in the the form of clusters for K-means clustering technique.
     '''
@@ -26,71 +26,40 @@ class generate_data:
         '''
         self.num_of_clusters = k
 
-    def data_generation(self,
-                        num_of_samples, 
+    @staticmethod
+    def data_generation(num_of_samples, 
                         X_Cooridinate_Start, 
                         X_Cooridinate_End, 
                         Y_Cooridinate_Start, 
                         Y_Cooridinate_End):
-        '''
-        This function creates sample coordinates.
-
-        Parameters
-        ------------
-        num_of_samples = int
-                        Number of observations required in a cluster
-        X_Cooridinate_Start = int
-                        Minimum X-cooridinate of an observation in the cluster
-        X_Cooridinate_End = int
-                        Maximum X-cooridinate of an observation in the cluster
-        Y_Cooridinate_Start = int
-                        Minimum Y-cooridinate of an observation in the cluster
-        Y_Cooridinate_End = int
-                        Maximum Y-cooridinate of an observation in the cluster
-
-        Returns
-        -------------
-        return : Arraylist
-                Two list each for X-coordinates and Y-coordinates of observations in the cluster
-        '''
+        
         cluster_X = numpy.random.random(size=(num_of_samples))
         cluster_X = cluster_X * (X_Cooridinate_End - X_Cooridinate_Start) + X_Cooridinate_Start
         cluster_Y = numpy.random.random(size=(num_of_samples))
         cluster_Y = cluster_Y * (Y_Cooridinate_End - Y_Cooridinate_Start) + Y_Cooridinate_Start
         return cluster_X, cluster_Y
 
-    def raw_data(self):
-        '''
-        This function generates cluster of obervations/data-points based on the input of the user.
-
-        Returns
-        -----------
-        return : Dict
-                A dictionary of cluster.
-
-        Output example
-        ---------------
-        {'cluster_0': [[<list-of-X-coordinates],[<list-of-Y-coordinates]],
-                'cluster_1': [[<list-of-X-coordinates],[<list-of-Y-coordinates]],
-                ..
-                ..
-                'cluster_n': [[<list-of-X-coordinates],[<list-of-Y-coordinates]] }
-        '''
-        k = self.num_of_clusters
-        cluster_map = {}
-        input_list = []
-        for i in range(k):
-            input_list.append([random.randint(25,60),random.randint(0,100),random.randint(20,100),random.randint(0,100),random.randint(20,100)])
-        
-        for i in range(len(input_list)):
-            X_coordinate_list = self.data_generation(input_list[i][0],input_list[i][1],input_list[i][2],
-                                                input_list[i][3],input_list[i][4])[0]
-            Y_coordinate_list = self.data_generation(input_list[i][0],input_list[i][1],input_list[i][2],
-                                                input_list[i][3],input_list[i][4])[1]
-            cluster_map[f'cluster_{i}'] = [X_coordinate_list,Y_coordinate_list]
-        return cluster_map
-
     
+    def raw_data(self):
+        data = []
+        k = self.num_of_clusters
+        if k == 2:
+            c1 = numpy.array([self.data_generation(10,0,5,2,6)]).T
+            c2 = numpy.array([self.data_generation(10,4,12,14,18)]).T
+            data = numpy.concatenate((c1, c2), axis=0)
+
+        elif k == 3:
+            c1 = numpy.array([self.data_generation(10,0,5,2,6)]).T
+            c2 = numpy.array([self.data_generation(10,4,12,14,18)]).T
+            c3 = numpy.array([self.data_generation(10,25,35,50,90)]).T
+            data = numpy.concatenate((c1, c2, c3), axis=0)
+
+        else:
+            print("Out of Scope")   
+
+        return data
+
+        
     def plot_input(self,input_data):
         '''
         This function plots the input data on a 2-D plane.
@@ -111,7 +80,7 @@ class generate_data:
 
         return matplotlib.pyplot.show()
     
-    def raw_to_standard(self, raw_data):
+    """  def raw_to_standard(self, raw_data):
         i = 0
         true_labels = []
         l = []
@@ -122,5 +91,5 @@ class generate_data:
             i+=1       
         true_labels = numpy.array(true_labels)
         l = numpy.array(l)
-        return l, true_labels
+        return l, true_labels """
     
