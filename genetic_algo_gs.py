a87d5f9617069dc48dd4c771e3d74b55738f318e
gurpreetsingh14 <gps.off.14@gmail.com>
2022-12-03 15:58:32 -0500

    Updates 12

--- genetic_algo_gs.py
+++ genetic_algo_gs.py
@@ -1,47 +1,15 @@
+#Import Libraries
 import numpy
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
+from generate_data_class import GenerateData
+import kmeans
 
-# ARTIFICIAL DATA GENERATION
-
-cluster1_num_samples = 10
-cluster1_x1_start = 0
-cluster1_x1_end = 5
-cluster1_x2_start = 2
-cluster1_x2_end = 6
-cluster1_x1 = numpy.random.random(size=(cluster1_num_samples))
-cluster1_x1 = cluster1_x1 * (cluster1_x1_end - cluster1_x1_start) + cluster1_x1_start
-cluster1_x2 = numpy.random.random(size=(cluster1_num_samples))
-cluster1_x2 = cluster1_x2 * (cluster1_x2_end - cluster1_x2_start) + cluster1_x2_start
-
-cluster2_num_samples = 10
-cluster2_x1_start = 4
-cluster2_x1_end = 12
-cluster2_x2_start = 14
-cluster2_x2_end = 18
-cluster2_x1 = numpy.random.random(size=(cluster2_num_samples))
-cluster2_x1 = cluster2_x1 * (cluster2_x1_end - cluster2_x1_start) + cluster2_x1_start
-cluster2_x2 = numpy.random.random(size=(cluster2_num_samples))
-cluster2_x2 = cluster2_x2 * (cluster2_x2_end - cluster2_x2_start) + cluster2_x2_start
-
-c1 = numpy.array([cluster1_x1, cluster1_x2]).T
-c2 = numpy.array([cluster2_x1, cluster2_x2]).T
-
-data = numpy.concatenate((c1, c2), axis=0)
-
-#data
-# the numpy lists 1st 10 list are X,Y coordinates of cluster 1 points and rest 10 list are 
-# X,Y coordinates of cluster 2 points
-
+#Set Constant Variables
 num_clusters = 2
 
-'''
-euclidean_distance() accepts 2 inputs X and Y. 
-One of these inputs can be a 2-D array with multiple samples, and the other input should be a 1-D array with just 1 sample. 
-The function calculates and returns the Euclidean distances between each sample in the 2-D array and 
-the single sample in the 1-D array
-'''
-def euclidean_distance(X, Y):
-    return numpy.sqrt(numpy.sum(numpy.power(X - Y, 2), axis=1))
+#Artificial Data Generation
+gy = GenerateData(num_clusters)
+data = gy.raw_data()
 
 '''
 Input: 
@@ -62,7 +30,7 @@ def cluster_data(solution):
 
     for clust_idx in range(num_clusters):
         cluster_centers.append(solution[feature_vector_length*clust_idx:feature_vector_length*(clust_idx+1)])
-        cluster_center_dists = euclidean_distance(data, cluster_centers[clust_idx])
+        cluster_center_dists = kmeans.euclidean(data, cluster_centers[clust_idx])
         all_clusters_dists.append(numpy.array(cluster_center_dists))
 
     cluster_centers = numpy.array(cluster_centers)
@@ -96,23 +64,40 @@ def fitness_func(solution):
 
 #GENERATE Initial coordinates for cluster center
 
-def init_cluster_center(num_clusters,start_coord,end_coord):
+def init_cluster_center(num_clusters):
     io = []
-    rc = [2,2.25,2.5,2.75,3,3.25,3.5,3.75,4]
-    cluster1_num = num_clusters*2
-    cluster1_x1_start = start_coord
-    cluster1_x1_end = end_coord
-    cluster1_x1 = numpy.random.random(size=(cluster1_num))
-    cluster1_x1 = cluster1_x1 * (cluster1_x1_end - cluster1_x1_start) + cluster1_x1_start
-    io.append(cluster1_x1)
+    rc = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]
+    if num_clusters == 3:
+        cluster1_x1 = numpy.array([40,46,11,11,2,2])
+    elif num_clusters == 2:
+        cluster1_x1 = numpy.array([2, 3, 9, 15])
+    else:
+        print("Out od Scope")
+
     for y in range(len(rc)):
         ui = rc[y]*cluster1_x1
         io.append(ui)
     return numpy.array(io)
-# return: numpy array: [[C1-x C1-y C2-x C2-y][][][]....]
 
-#SELECTION FUNCTION
+#Print Result
+def get_results(generation,population,fitness):
+    m = population.shape[0]
+    best = [fitness.max()]
+    index = numpy.where(numpy.isclose(fitness, best))
+    population = numpy.array(population)
+    print(f'Generation #{generation}   |fitness: {max(fitness):0.5f} |Centroid = {population[index[0]][0]}')
 
+#Plot Fitness Values over generations
+def display_plot(best):
+    plt.plot(best, color='c')
+    plt.xlabel('Iteration')
+    plt.ylabel('Best Fitness')
+    plt.grid()
+    plt.show()
+
+#Steps in Genetic Algorithm
+
+#1. Selection
 def selection(pop,sample_size, fitness):
     m,n = pop.shape
     new_pop = pop.copy()
@@ -124,8 +109,7 @@ def selection(pop,sample_size, fitness):
     
     return new_pop
 
-#CROSSOVER
-
+#2. Crossover
 def crossover(pop, pc):
     m,n = pop.shape
     new_pop = pop.copy()
@@ -138,49 +122,29 @@ def crossover(pop, pc):
             
     return new_pop
 
-#MUTATION
-
+#3. Mutation
 def mutation(pop, pm):
     m,n = pop.shape
     new_pop = pop.copy()
     mutation_prob = (numpy.random.uniform(0, 1, size=(m,n)) < pm).astype(int)
     return (mutation_prob + new_pop) % 2
 
-#GENETIC ALGORITHM
 
-def GeneticAlgorithm(func, num_clusters,start_coord,end_coord, 
-                     ps=0.2, pc=1.0, pm=0.1, max_iter=1, random_state=None):
-    
+def GeneticAlgorithm(func, num_clusters,ps=0.2, pc=1.0, pm=0.1, max_iter=1, random_state=1234):    
     numpy.random.seed(random_state)
-    pop = init_cluster_center(num_clusters,start_coord,end_coord)
+    pop = init_cluster_center(num_clusters)
     fitness = func(pop)
-    best = [fitness.max()]
-    fitness_res = []
+    best = [fitness.max()]    
+    print('=' * 68)
+    get_results(-1,pop,fitness)
     i = 0
-    #print('==============Population before SELECTION')
-    #print(pop)
-    #print('==============Fitness list')
-    #print(fitness)
     while i < max_iter:
         pop = selection(pop, ps, fitness)
-        print('==============Population after SELECTION')
-        print(pop)
         pop = crossover(pop, pc)
-        print('==============Population after CROSSOVER')
-        print(pop)
         pop = mutation(pop, pm)
-        print('==============Population after MUTATION')
-        print(pop)
         fitness = func(pop)
-        fitness_res.append(fitness)
+        best.append(fitness.max())
+        get_results(i,pop,fitness)
         i += 1
-    
-    #print(f'Generation {i} fitness {fitness:0.4f}')    
-    #print(f'Best fitness: {max(fitness_res):0.4f} at generation {fitness_res.index(max(fitness_res))}')
-            
-    return fitness_res, best, pop
-
-f,b,p = GeneticAlgorithm(fitness_func,2,0,20)
-print(f'fitness is: {f}')
-print(f'best fitness is: {b}')
-print(f'population of centroids is: {p}')
+        
+    return fitness, best, i, pop
